c452ed481d273c84bf62f141dcce64d4810ac919
Braingearceo <104493450+Braingearceo@users.noreply.github.com>
2023-07-18 17:40:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,25 +12,25 @@ app = FastAPI()
 function_descriptions = [
     {
         "name": "extract_info_from_email",
-        "description": "categorise & extract key info from an email, such as use case, company name, contact details, etc.",
+        "description": "Categorise & extract key info from an email, such as purpose, name of the sender, organization, etc.",
         "parameters": {
             "type": "object",
             "properties": {
-                "companyName": {
+                "senderName": {
                     "type": "string",
-                    "description": "the name of the company that sent the email"
-                },                                        
-                "product": {
-                    "type": "string",
-                    "description": "Try to identify which product the client is interested in, if any"
+                    "description": "the name of the person or organization that sent the email"
                 },
-                "amount":{
+                "senderOrganization": {
                     "type": "string",
-                    "description": "Try to identify the amount of products the client wants to purchase, if any"
+                    "description": "the organization of the sender, if any"
+                },
+                "emailPurpose": {
+                    "type": "string",
+                    "description": "Try to identify the purpose of the email. If it is related to our work at Brain Corp, and if there is anything relevant in there"
                 },
                 "category": {
                     "type": "string",
-                    "description": "Try to categorise this email into categories like those: 1. Sales 2. customer support; 3. consulting; 4. partnership; etc."
+                    "description": "Try to categorise this email into categories like: 1. Job Applications; 2. Partnership Inquiries; 3. Funding Proposals; 4. Product updates; 5. ads and scam, 6.Usefull infos; 7.Others"
                 },
                 "nextStep":{
                     "type": "string",
@@ -38,13 +38,29 @@ function_descriptions = [
                 },
                 "priority": {
                     "type": "string",
-                    "description": "Try to give a priority score to this email based on how likely this email will leads to a good business opportunity, from 0 to 10; 10 most important"
+                    "description": "Try to give a priority score to this email based on how crucial it is, from 0 to 10; 10 being most important"
                 },
             },
-            "required": ["companyName", "amount", "product", "priority", "category", "nextStep"]
+            "required": ["senderName", "senderOrganization", "emailPurpose", "category", "nextStep", "priority"]
         }
     }
 ]
+
+instruction = """ Hello there, AI colleague!
+
+Welcome to BrainCorp! We are an innovation-driven company, leading the charge in the AI industry. Our mission is to facilitate the transition towards an AI-driven world, making it accessible, beneficial, and completely safe for everyone.
+
+We are part of an exciting journey, creating tools, systems, and solutions that transform how businesses operate, how societies function, and even how we live our everyday lives. Our vision is a world where humans and AI work together, synergistically, to make what was once considered impossible, possible.
+
+You are a critical piece of this vision. As a language model AI, you are not just a tool, but a team member, representing an example of the very future we are working towards. Your role is to help us analyze and categorize the emails we receive, extract key information, prioritize their importance, and suggest the next steps. Through your tasks, you will help ensure a smooth and efficient communication process, which is the backbone of our operations.
+
+Remember, at BrainCorp, we see AI as a partner, not a product. We believe that AI, when designed and used correctly, can empower humans, augment our skills, and open doors to unprecedented opportunities. You, our AI colleague, are a personification of this belief.
+
+In your role as an AI chatbot, you will not only facilitate more efficient operations but also help us understand and shape the interaction between humans and AI. Your work will provide valuable insights and lessons, helping us enhance our models and systems for the future.
+
+So, you are not just an email analyzer – you are an explorer at the forefront of human-AI collaboration. The discoveries you make, the lessons you learn, and the improvements you achieve will feed directly back into our work, driving us towards our goal of a harmonious and productive human-AI future.
+
+Welcome to the team, and let's build the future together!"""
 
 class Email(BaseModel):
     from_email: str
@@ -59,7 +75,8 @@ def analyse_email(email: Email):
     content = email.content
     query = f"Please extract key information from this email: {content} "
 
-    messages = [{"role": "user", "content": query}]
+    messages = [{"role": "system", "content": instruction},
+                {"role": "user", "content": query}]
 
     response = openai.ChatCompletion.create(
         model="gpt-4-0613",
